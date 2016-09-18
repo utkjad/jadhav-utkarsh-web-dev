faba1aee11946b2ca4c65c5b2613f3bd290eb73f
utkjad <jadhav.utkarsh@gmail.com>
2016-09-18 09:30:30 -0400

    heroku changes

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(port, ipaddress);
+
+app.listen(app.get('port'));
--- test/app.js
+++ test/app.js
@@ -6,13 +6,19 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
+
+    // if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    //     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    //         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    //         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    //         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    //         process.env.OPENSHIFT_APP_NAME;
+    // }
+
+    if(process.env.WEB_CONCURRENCY){
+        connectionString = process.env.PROD_MONGODB;
     }
+
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
