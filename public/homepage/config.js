853575f4a2f283fa25578069a3171ac4e85252cc
utkjad <jadhav.utkarsh@gmail.com>
2016-11-14 00:37:42 -0500

    ```Homepage stuff 2

--- public/homepage/config.js
+++ public/homepage/config.js
@@ -8,5 +8,19 @@
             .when("/aboutme", {
                 templateUrl: "views/about-me.view.client.html"
             })
+            .when("/resume", {
+                templateUrl: "views/resume.view.client.html"
+            })
+            .when("/contact", {
+                templateUrl: "views/contact.view.client.html"
+            })
+            .when("/quotes", {
+                templateUrl: "views/quotes.view.client.html"
+            })
+            // Default case
+            .otherwise({
+                    redirectTo: '/aboutme'
+                }
+            );
     }
 })();
