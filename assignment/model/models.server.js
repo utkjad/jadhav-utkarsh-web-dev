50c21f2d803e370d3654aeed33fc4fdd3a15d209
utkjad <jadhav.utkarsh@gmail.com>
2016-12-03 18:24:50 -0500

    make things work on heroku

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,8 +1,23 @@
 module.exports = function () {
     console.log("In SERVER models.server.js");
 
+    var connectionString = 'mongodb://127.0.0.1:27017/test';
+
+
+    // if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    //     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    //         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    //         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    //         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    //         process.env.OPENSHIFT_APP_NAME;
+    // }
+
+    if(process.env.WEB_CONCURRENCY){
+        connectionString = process.env.MONGODB_URI;
+    }
     var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/wam-fall-2016');
+    mongoose.connect(connectionString);
+    // 'mongodb://localhost/wam-fall-2016');
 
     var userModel = require("./user/user.model.server")();
     var websiteModel = require("./website/website.model.server")();
